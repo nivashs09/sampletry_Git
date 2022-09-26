6e1a6e7cf7e2f44112bc419042e1d287a878bf65
Gha <nivashsrmece@gmail.com>
2022-09-26 11:43:18 +0530

    Signed-off-by: Gha <nivashsrmece@gmail.com>

---
+++ Sampletry/src/packing/multiple.java
@@ -0,0 +1,19 @@
+package packing;
+
+public class multiple {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int num1,num2,c,mul;
+			num1=2;
+			num2=4;
+			c=4;
+			mul= num1*num2*c;
+			System.out.println("Multiply=" +mul);
+
+		}
+
+	}
+
+
+
